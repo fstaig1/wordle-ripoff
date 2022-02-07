5149985e200f5c117f28e7fd90ab2687dc4e6f1d
Finlay Staig <91324517+fstaig1@users.noreply.github.com>
2022-02-07 17:55:10 +0000

    fixed some linter errors

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import random as r
 import os
 
 
-def clear(): os.system("cls") # function to let me easily clear the screen
+# function to let me easily clear the screen
+def clear():
+    os.system("cls")
 
 
 # constants to store file locations
-WORD_FILE = "wordle-ripoff\data\sgb-words.txt"
+WORD_FILE = "wordle-ripoff\\data\\sgb-words.txt"
+
 
 # constants to store word list(ik they're technically not constants cos i instantly edit them but they never change after that)
 WORD_LIST = []
-
 
 # admin test vars
 ADMIN_WORD = "wheat"
@@ -21,9 +23,10 @@ with(open(WORD_FILE) as file):
     for line in file:
         WORD_LIST.append(line.split("\n")[0].split(" ")[0])
 
+
 # pick word from answer list
 def generateWord():
-    return(WORD_LIST[r.randrange(0,len(WORD_LIST)-1)])
+    return(WORD_LIST[r.randrange(0, len(WORD_LIST) - 1)])
 
 
 # initialise game stuff idk yet
@@ -31,29 +34,30 @@ def initGame():
     global currentWord
     if not ADMIN_WORD:
         currentWord = generateWord()
-    else: currentWord = ADMIN_WORD
-    
+    else:
+        currentWord = ADMIN_WORD
+
 
 # user guessing
 def guessWord():
-    for guessNum in range(1,7):
+    for guessNum in range(1, 7):
         valid = False
-        while valid == False:
-            print(WORD_LIST[r.randrange(0,len(WORD_LIST)-1)])
-            print("Guess number %s." % guessNum)
+        while valid is False:
+            print("\nGuess #%s." % guessNum)
+            print("/ %s /" % generateWord())  # TODO remove
             guess = input("> ").lower()
-            if guess in WORD_LIST: 
+            if guess in WORD_LIST:
                 valid = True
-            else: print("enter a valid 5 letter word")
-    
+            else:
+                print("enter a valid 5 letter word")
 
-# main  
+
+# main
 def main():
     initGame()
     guessWord()
-    
-    
+
 
 main()
-    
+
 input("pause debugger :)")
