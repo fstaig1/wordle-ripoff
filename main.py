27dc3a8e810351a452210cae1511b1c19844e9d9
Finlay Staig <91324517+fstaig1@users.noreply.github.com>
2022-02-07 17:47:40 +0000

    🙏praise you donald e knuth🙏
    deleted a broken dataset, added a new working one
    
    link to dataset - https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 import random as r
+import os
 
 
-WORD_FREQ_FILE = "wordle-ripoff\\data\\word freq.txt"
-WORD_FILE = "wordle-ripoff\\data\\words.txt"
-ANSWER_FILE = "wordle-ripoff\\data\\answers.txt"
-ANSWER_FREQ_FILE = "wordle-ripoff\\data\\answer freq.txt"
-WORD_FREQ_LIST = []
+def clear(): os.system("cls") # function to let me easily clear the screen
+
+
+# constants to store file locations
+WORD_FILE = "wordle-ripoff\data\sgb-words.txt"
+
+# constants to store word list(ik they're technically not constants cos i instantly edit them but they never change after that)
 WORD_LIST = []
-ANSWER_LIST = []
-ANSWER_FREQ_LIST = []
-with(open(WORD_FREQ_FILE) as file):
+
+
+# admin test vars
+ADMIN_WORD = "wheat"
+
+
+# generate word lists
+with(open(WORD_FILE) as file):
     for line in file:
-        WORD_FREQ_LIST.append(line.split("\n")[0].split(" "))
         WORD_LIST.append(line.split("\n")[0].split(" ")[0])
 
-
-with(open(ANSWER_FREQ_FILE) as file):
-    for line in file:
-        ANSWER_FREQ_LIST.append(line.split("\n")[0].split(" "))
-        ANSWER_LIST.append(line.split("\n")[0].split(" ")[0])
+# pick word from answer list
+def generateWord():
+    return(WORD_LIST[r.randrange(0,len(WORD_LIST)-1)])
 
 
-def chooseWord():
-    return(ANSWER_LIST[r.randrange(0,len(ANSWER_LIST)-1)])
-
-
+# initialise game stuff idk yet
 def initGame():
-    currentWord = chooseWord()
-    return currentWord
+    global currentWord
+    if not ADMIN_WORD:
+        currentWord = generateWord()
+    else: currentWord = ADMIN_WORD
     
+
+# user guessing
+def guessWord():
+    for guessNum in range(1,7):
+        valid = False
+        while valid == False:
+            print(WORD_LIST[r.randrange(0,len(WORD_LIST)-1)])
+            print("Guess number %s." % guessNum)
+            guess = input("> ").lower()
+            if guess in WORD_LIST: 
+                valid = True
+            else: print("enter a valid 5 letter word")
     
+
+# main  
 def main():
     initGame()
+    guessWord()
     
-    print("pause debugger :)")
+    
 
 main()
+    
+input("pause debugger :)")
