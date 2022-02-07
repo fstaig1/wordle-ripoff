04c2d36209276876971e7ad7a06000314fa43482
Finlay Staig <91324517+fstaig1@users.noreply.github.com>
2022-02-07 16:51:53 +0000

    updated the data sets a bit
    ive taken them from here
    
    https://github.com/3b1b/videos/tree/master/_2022/wordle_data

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 
-from pkg_resources import yield_lines
-
-
-WORD_FREQ_FILE = "wordle-ripoff\data\word freq.txt"
-WORD_FILE = "wordle-ripoff\data\words.txt"
+WORD_FREQ_FILE = "wordle-ripoff\\data\\word freq.txt"
+WORD_FILE = "wordle-ripoff\\data\\words.txt"
 ANSWER_FILE = "wordle-ripoff\\data\\answers.txt"
+ANSWER_FREQ_FILE = "wordle-ripoff\\data\\answer freq.txt"
 
 WORD_FREQ_LIST = []
 WORD_LIST = []
 ANSWER_LIST = []
+ANSWER_FREQ_LIST = []
 
 with(open(WORD_FREQ_FILE) as file):
     for line in file:
         WORD_FREQ_LIST.append(line.split("\n")[0].split(" "))
         WORD_LIST.append(line.split("\n")[0].split(" ")[0])
 
-with(open(ANSWER_FILE) as file):
+with(open(ANSWER_FREQ_FILE) as file):
     for line in file:
-        ANSWER_LIST.append(line)
+        ANSWER_FREQ_LIST.append(line.split("\n")[0].split(" "))
+        ANSWER_LIST.append(line.split("\n")[0].split(" ")[0])
 
+print("pause debugger :)")
